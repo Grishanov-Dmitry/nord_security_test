83b899299be28bcb92f2ba67a082d8a1aecaab36
dmitry-grishanov <grishanov.ds@gmail.com>
2023-09-12 12:21:41 +0200

    feature: Add navigate to root in case with absence token

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,32 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 
 import styles from "./Layout.module.css";
+import { useSelector } from "react-redux";
+import { getToken } from "../../features/serverSelectors";
 
 interface ILayout {
   children: ReactNode;
 }
 
-export const Layout = ({ children }: ILayout) => (
-  <div className={styles.layoutContainer}>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+export const Layout = ({ children }: ILayout) => {
+  const token = useSelector(getToken);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
+  return (
+    <div className={styles.layoutContainer}>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
